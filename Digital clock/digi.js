6e290e1f3c6446e9a2cd210a12c1c1c1905c44bd
jaimin2426 <jaiminp2k6@gamil.com>
2025-12-15 20:55:48 +0530

    digital clock

---
+++ Digital clock/digi.js
@@ -0,0 +1,11 @@
+const clock = document.getElementById('clock')
+// const clock = document.querySelector('#clock')
+
+
+
+
+setInterval(function() {
+    let date = new Date();
+    // console.log(data.toLocaleString());
+    clock.innerHTML = date.toLocaleTimeString();
+} , 1000)
